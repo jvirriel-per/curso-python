3eb1fb28f68106de69aff29f6347926ac408179e
José Virriel <38949440+jvirriel-per@users.noreply.github.com>
2018-05-14 11:54:39 -0400

    commit

---
+++ Enteros-reales.py
@@ -0,0 +1,25 @@
+a = 26
+b = 11.3
+c = 5
+d = 3.5
+
+#SUMA
+print (a+b)
+
+#RESTA
+print (c-a)
+
+#MULTIPLICACION
+print (d*c)
+
+#EXPONENTE
+print (c**2)
+
+#DIVISION
+print (c/a)
+
+#DIVISION ENTERA
+print (c/a)
+
+#MODULO
+print (7%3)
