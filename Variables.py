38e8b93eb627ed7457bd3386a147bc96583ff3c3
José Virriel <38949440+jvirriel-per@users.noreply.github.com>
2018-05-14 11:13:15 -0400

    commit

---
+++ Variables.py
@@ -0,0 +1,10 @@
+#Para declarar variables en python la regla de oro es escribirla totalmente en minuscula
+#si necesitamos unir palabras usamos "_" para unirlas 
+
+esto_es_una_variable = ("Asi almacenamos este string en la variable")
+un_entero_en_una_variable = 80
+un_booleano_en_una_variable = True
+
+print (esto_es_una_variable)
+print (un_entero_en_una_variable)
+print (un_booleano_en_una_variable)
